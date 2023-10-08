bc1d41a199962d05fec59e8d99acb26161519218
soukaynabensalah <146835722+soukaynabensalah@users.noreply.github.com>
2023-10-09 00:16:17 +0100

    Create Monthnbrs.py

---
+++ Monthnbrs.py
@@ -0,0 +1,19 @@
+mois=int("input("entrer le nbr de mois :"))
+match mois :
+    case 1 :31
+    case 2 :28 or 29
+    case 3 :30
+    case 4 :31
+    case 5 :30
+    case 6 :31
+    case 7 :31
+    case 8 :30
+    case 9 :31
+    case 10:30
+    case 11 :31
+    case 12 :30
+   
+   
+    
+
+  
